cba2277badd56f5154ba499541828812d6e03708
Doychin Atanasov <ironsmile@gmail.com>
2021-08-01 22:58:52 +0300

    Add audio capabilities with gstreamer
    Actually, this is my very first gstreamer program so everything is kind
    of experimental. Once I get the hang of it there will be quite a lot of
    polishing and moving conde into its own player module.

--- src/window.py
+++ src/window.py
@@ -37,19 +37,118 @@ class EuterpeGtkWindow(Gtk.ApplicationWindow):
     about_gstreamer_version = Gtk.Template.Child()
     about_python_version = Gtk.Template.Child()
 
+    input_token = Gtk.Template.Child()
+    input_track_url = Gtk.Template.Child()
+    play_button = Gtk.Template.Child()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.squeezer.connect("notify::visible-child",self.on_headerbar_squeezer_notify)
+        self.squeezer.connect("notify::visible-child",
+                              self.on_headerbar_squeezer_notify)
+        self.play_button.connect("clicked",
+                              self.on_play_button_clicked)
+        self.input_token.connect("changed",
+                              self.on_token_changed)
+        self.input_track_url.connect("changed",
+                              self.on_track_changed)
+
         Gst.init(None)
 
-        self.populate_about()
+        self.play_uri = None
+        self.token = None
 
+        self.populate_about()
+        self.playbin = None
+
+    def on_token_changed(self, entry):
+        text = entry.get_text()
+        if len(text) > 0:
+            self.token = text
+        else:
+            self.token = None
+
+    def on_track_changed(self, entry):
+        text = entry.get_text()
+        if len(text) > 0:
+            self.play_uri = text
+        else:
+            self.play_uri = None
 
     def on_headerbar_squeezer_notify(self, squeezer, event):
 	    child = squeezer.get_visible_child()
 	    self.bottom_switcher.set_reveal(child != self.headerbar_switcher)
 
+    def on_play_button_clicked(self, button):
+        print("play button clicked")
+
+        if self.play_uri is None:
+            print("no play URI!")
+            return
+
+        if self.token is None:
+            print("no token!")
+            return
+
+        if self.playbin is not None:
+            self.playbin.set_state(Gst.State.NULL)
+            self.playbin = None
+
+        pipeline = Gst.Pipeline.new('mainpipeline')
+
+        src = Gst.ElementFactory.make("curlhttpsrc", "source");
+        src.set_property('location', self.play_uri)
+
+        headers = Gst.Structure.new_empty('extra-headers')
+        headers.set_value("Authorization", "Bearer " + self.token)
+        src.set_property('extra-headers', headers)
+
+        dec = Gst.ElementFactory.make("decodebin", "decoder")
+
+        pipeline.add(src)
+        pipeline.add(dec)
+        src.link(dec)
+
+        audio = Gst.Bin.new('audiobin')
+        conv = Gst.ElementFactory.make("audioconvert", "aconv")
+        audiopad = conv.get_static_pad("sink")
+        sink = Gst.ElementFactory.make("autoaudiosink", "output")
+        audio.add(conv)
+        audio.add(sink)
+        conv.link(sink)
+        audio.add_pad(Gst.GhostPad.new('sink', audiopad))
+        pipeline.add(audio)
+
+        dec.connect("pad-added", self._on_newpad, audio)
+
+        bus = pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message::error", self._on_bus_error)
+        bus.connect("message::eos", self._on_bus_eos)
+        bus.connect("message::stream-start", self._on_stream_start)
+
+        pipeline.set_state(Gst.State.PLAYING)
+        self.playbin = pipeline
+
+    def _on_newpad(self, dec, pad, audiosink):
+        print("newpad called")
+        #TODO: check caps!
+        audiopad = audiosink.get_static_pad("sink")
+        pad.link(audiopad)
+
+    def _on_bus_eos(self, bus, message):
+        print("playbin message: EOS")
+        self.playbin.set_state(Gst.State.NULL)
+        self.playbin = None
+
+    def _on_stream_start(self, bus, message):
+        print("playbin stream_start", message)
+
+    def _on_bus_error(self, bus, message):
+        (error, parsed) = message.parse_error()
+        # App().notify.send("Lollypop", parsed)
+        print("playbin error:", parsed)
+        self.playbin.set_state(Gst.State.NULL)
 
     def populate_about(self):
         self.about_python_version.set_label('{}.{}.{}'.format(
